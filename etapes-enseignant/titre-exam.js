d1b15ab1619dde916b875c28635c5dad3b80bc75
chaimae akayour <chaimae.akayour@etu.uae.ac.ma>
2025-05-03 22:41:34 +0200

    ajoute titre exam

---
+++ etapes-enseignant/titre-exam.js
@@ -0,0 +1,17 @@
+
+function submitExamForm(event) {
+    event.preventDefault();
+  
+    const titre = document.getElementById('titre').value;
+    const semestre = document.getElementById('semestre').value;
+    const filiere = document.getElementById('filiere').value;
+  
+    
+    localStorage.setItem('examTitre', titre);
+    localStorage.setItem('examSemestre', semestre);
+    localStorage.setItem('examFiliere', filiere);
+  
+    
+    window.location.href = 'exam.html';
+  }
+  
