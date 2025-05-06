2bb37fde42a36a0a4e295b94f85fd0436b6f91c5
chaimae akayour <chaimae.akayour@etu.uae.ac.ma>
2025-05-06 22:55:08 +0200

    modifier js creation examen

--- etapes-enseignant/titre-exam.js
+++ etapes-enseignant/titre-exam.js
@@ -1,20 +1,23 @@
-
 function submitExamForm(event) {
-    event.preventDefault();
+  event.preventDefault();
+
+  const titre = document.getElementById('titre').value;
+  const semestre = document.getElementById('semestre').value;
+  const filiere = document.getElementById('filiere').value;
+
   
-    const titre = document.getElementById('titre').value;
-    const semestre = document.getElementById('semestre').value;
-    const filiere = document.getElementById('filiere').value;
-  
-    
-    localStorage.setItem('examTitre', titre);
-    localStorage.setItem('examSemestre', semestre);
-    localStorage.setItem('examFiliere', filiere);
-  
-    
-    window.location.href = 'exam.html';
-  }
-  document.getElementById("logoutBtn").addEventListener("click", () => {
-    alert("Déconnexion réussie !");
-    
-  });
+  localStorage.setItem('examTitre', titre);
+  localStorage.setItem('examSemestre', semestre);
+  localStorage.setItem('examFiliere', filiere);
+
+
+  window.location.href = 'page-cree-examen.html';
+}
+
+
+document.getElementById("examForm").addEventListener("submit", submitExamForm);
+
+
+document.getElementById("logoutBtn").addEventListener("click", () => {
+  alert("Déconnexion réussie !");
+});
