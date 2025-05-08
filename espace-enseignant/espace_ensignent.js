0f6d5c9ac6d31253d70fa771ec90591c29904ef2
Amtout Raouda <raoudaamtout55@gmail.com>
2025-05-08 20:32:57 +0100

    espace ensignent finale update

---
+++ espace-enseignant/espace_ensignent.js
@@ -0,0 +1,36 @@
+
+   document.addEventListener("DOMContentLoaded", function () {
+
+  const firstName = localStorage.getItem("firstName");
+  const lastName = localStorage.getItem("lastName");
+  const userType = localStorage.getItem("userType");
+
+  if (!firstName || !lastName || userType !== "Enseignant") {
+    alert("Veuillez vous connecter en tant qu'enseignant.");
+    window.location.href = "form.html";
+    return;
+  }
+  else if(matchedUser.userType==='Enseignant'){
+    window.location.href = "espace_ensignent_interface.html";
+
+  }
+
+      document.getElementById("username").textContent =`${firstName} ${lastName}`;
+  
+  
+      const userInfoBtn = document.getElementById("userInfoBtn");
+      userInfoBtn.addEventListener("click", function () {
+        const dropdown = document.getElementById("userDropdown");
+        const arrow = document.querySelector(".arrow");
+        dropdown.style.display =
+          dropdown.style.display === "block" ? "none" : "block";
+        arrow.classList.toggle("up");
+      });
+  
+      
+      document.getElementById("logoutLink").addEventListener("click", function (e) {
+        e.preventDefault();
+        localStorage.clear();
+        window.location.href = "form.html";
+      });
+    });
