1525c86b848e31c4af80bfcf622b945db7904e46
chaimae akayour <chaimae.akayour@etu.uae.ac.ma>
2025-05-09 22:30:39 +0200

    change

--- titre-exam.js
+++ titre-exam.js
@@ -1,11 +1,13 @@
-// JavaScript
 function submitExamForm(event) {
   event.preventDefault();
 
   const titre = document.getElementById('titre').value;
 
-  
   localStorage.setItem('examTitre', titre);
+
+  const firstName = localStorage.getItem("firstName");
+  const lastName = localStorage.getItem("lastName");
+  document.getElementById("username").textContent = `${firstName} ${lastName}`;
 
   window.location.href = 'page-cree-examen.html';
 }
