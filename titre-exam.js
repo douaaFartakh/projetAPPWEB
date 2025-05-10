41856fb67940db9c1df638d5a498700c281891d7
chaimae akayour <chaimae.akayour@etu.uae.ac.ma>
2025-05-10 02:19:58 +0200

    commit user name

--- titre-exam.js
+++ titre-exam.js
@@ -1,13 +1,18 @@
+document.addEventListener("DOMContentLoaded", function () {
+  const firstName = localStorage.getItem("firstName");
+  const lastName = localStorage.getItem("lastName");
+
+  if (firstName && lastName) {
+    document.getElementById("username").textContent = `${firstName} ${lastName}`;
+  } else {
+    document.getElementById("username").textContent = "Nom d'utilisateur";
+  }
+});
+
 function submitExamForm(event) {
   event.preventDefault();
 
-  const titre = document.getElementById('titre').value;
-
-  localStorage.setItem('examTitre', titre);
-
-  const firstName = localStorage.getItem("firstName");
-  const lastName = localStorage.getItem("lastName");
-  document.getElementById("username").textContent = `${firstName} ${lastName}`;
+  const titre = document.getElementById('titre').value;  
 
   window.location.href = 'page-cree-examen.html';
 }
